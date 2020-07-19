25a0442cff9045d003c32a6f38c4c0ca59906fa9
unknown <cpmglorioso@gmail.com>
2020-07-19 21:58:44 +0800

    Mongo DB

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://gloriverse:112112@react-starter.mzqiz.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("DB Connected"))
   .catch((err) => console.error(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json("hello");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
